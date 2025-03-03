31c998120d3be24dd7ad29e496224787425e8453
rkemmey <rachel.kemmey@gmail.com>
2025-03-03 15:07:46 -0600

    game

--- app.js
+++ app.js
@@ -1,27 +1,6 @@
 console.log("HELLO ZULU PLATOON!")
 // Your function(s) should go here that will interact with the webpage or DOM
 
-// print guess to screen
-const guessing = (event) => {
-    event.preventDefault();
-    // get form info
-    const formData = new FormData(event.target);
-    const formatData = Object.fromEntries(formData);
-    //console.log(formatData);
-
-    // grab html
-    let output = document.getElementById('output')
-    // output.innerText = `${formatData.guess}`
-
-    // append the new guess to output
-    let newGuess = document.createElement('p');  
-    newGuess.innerText = formatData.guess;  
-    output.appendChild(newGuess);  // add to outpus div
-
-    // clear the input field
-    event.target.reset();
-}
-
 // generte inclusive random number
 function getRandomInt(min, max) {
     const minCeiled = Math.ceil(min);
@@ -30,10 +9,50 @@ function getRandomInt(min, max) {
 }
 
 let randomNumber = getRandomInt(1, 100)
+console.log(randomNumber)
 
-// get guess
-const getGuess = () => {
+// print guess to screen
+const guessing = (event) => {
+    event.preventDefault();
+
+    // grab html div
+    let output = document.getElementById('output')
+
+    // get guess convert to num
     const userInput = document.getElementById('user-guess').value; 
-    const userGuess = Number(userInput); // convert to a number
+    let userGuess = Number(userInput); // convert to a number
+
+    let newGuess = document.createElement('p'); 
+    newGuess.innerText = userGuess;
+
+    // text to go with guess
+    let text = '';
+    if (userGuess > randomNumber) {
+        text = "Too high! Try again.";
+    } else if (userGuess < randomNumber) {
+        text = "Too low! Try again.";
+    } else if (userGuess === randomNumber) {
+        text = "Congratulations! You guessed it! Generating new number...";
+        randomNumber = getRandomInt(1, 100);
+        console.log(randomNumber);
+        output.innerHTML = '';
+    } else {
+        text = "Please enter a valid number.";
+    }
+
+    let feedback = document.createElement('p');
+    feedback.innerText = text;
+
+    // append to html div
+    output.appendChild(newGuess);
+    output.appendChild(feedback);
     console.log(userGuess); 
-};
+
+    // clear the input field
+    event.target.reset();
+}
+
+
+
+
+
